eb5f57a1856bd45ed2cc9ed34b8b2ed114c92865
Kumar-CDACDelhi <mrankitgoyal@gmail.com>
2021-11-23 15:22:08 +0530

    created the project. now you guys can add now file in the project

---
+++ src/GITClass.java
@@ -0,0 +1,18 @@
+
+public class GITClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HI Git project");
+		System.out.println("Done by lead");
+		System.out.println("Hello Guys.");
+		
+		System.out.println("in staging area.");
+		
+		
+		
+
+	}
+
+}
